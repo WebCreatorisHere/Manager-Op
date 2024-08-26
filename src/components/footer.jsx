db7eebaf89ad8c58b5be80dc209e9d0292115379
WebCreatorisHere <pratibhad.amit@gmail.com>
2024-08-26 17:07:38 +0530

    Manager op

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <div className=' text-center w-full text-white bg-[#121825] font-medium'>
+        <div className="text flex justify-center items-center flex-col"><h1 className='font-bold text-xl  text-white'><span className='text-green-600'>&lt;</span>Pass<span className='text-green-600'>OP&gt;</span></h1></div>
+        © 2024 Created with ❤ by Yash</div>
+  )
+}
+
+export default footer
